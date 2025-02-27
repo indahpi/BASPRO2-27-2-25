0e29ad0750a56a1edeb1a6a343dbed15be5341ab
Indah Purnamawati <indahpurnamawati2112@gmail.com>
2025-02-27 21:19:19 +0700

    Add files via upload

---
+++ VOLUME TABUNG.py
@@ -0,0 +1,7 @@
+print("Menghitung Volume Tabung")
+
+r = int(input("masukkan nilai jari-jari : "))
+t = int(input("masukkan nilai tinggi : "))
+V = 3.14 * r * r * t
+
+print("Volume Tabung adalah : ", V)
